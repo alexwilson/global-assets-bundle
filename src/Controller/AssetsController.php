1ae5cca79ccc614945f26921100caf5f76b949f3
Alex Wilson <a@ax.gy>
2017-03-20 02:13:10 +0000

    Document and fragment AssetsController.

--- src/Controller/AssetsController.php
+++ src/Controller/AssetsController.php
@@ -4,22 +4,45 @@ namespace antoligy\GlobalAssetsBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Twig_Environment;
 
 class AssetsController
 {
+    /**
+     * @var Twig_Environment
+     */
+    private $twig;
 
-    public function __construct($twig)
+    /**
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * @param Twig_Environment $twig Twig Environment, preconfigured by Symfony.
+     */
+    public function __construct(Twig_Environment $twig)
     {
         $this->twig = $twig;
+        $this->response = new Response();
+
+        $this->response->setPublic();        
+        $this->response->setSharedMaxAge(3600);
+        $this->response->setVary('Accept-Encoding');
     }
 
-    public function assetsAction(
-        Request $request,
-        $blk,
-        $tpl
-    ) {
+    /**
+     * Renders a block from a given template through ESI.
+     * Note: This relies upon Symfony's salted fragment handling,
+     * else it is possible to spoof the location of a dodgy template.
+     *
+     * @param string $blk Twig Block to render, containing assets.
+     * @param string $tpl Twig Template to render, containing our block.
+     */
+    public function assetsAction($blk, $tpl)
+    {
         $content = $this->twig->loadTemplate($tpl)->renderBlock($blk, []);
-        $response = new Response($content);
-        return $response;
+        $this->response->setContent($content);
+        return $this->response;
     }
 }
